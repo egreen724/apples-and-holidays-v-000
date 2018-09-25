503c70d8cdcabf0b981d3de11a7780cf123bd3f8
Erin Green <erin.y.green@gmail.com>
2018-09-25 01:07:43 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,6 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday|
   season.to_s
   puts "#{season.capitalize}:"
+  holiday.to_s
   holiday.split.capitalize!.join(" ")
   puts "#{holiday_name}:"
   puts "#{holiday.values}"
