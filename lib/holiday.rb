71412d84bfaf0bc449b90dc9667ed731ef5114cb
Erin Green <erin.y.green@gmail.com>
2018-09-25 01:43:05 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
  holiday_hash.each do |season, holiday|
    holiday.each do |holiday_name, supplies_array|
-     supplies_array.each do |supply|
+     supplies_array.collect do |supply|
        if supply == "BBQ"
          return holiday_name
          
